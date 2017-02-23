619d0ae5f447df6247ef3f50b24af5dfe10e7134
hello_chenchen <welcomechenchen@gmail.com>
2017-02-24 00:45:54 +0800

    同步上库

--- CCSocket/app/src/main/java/com/hello_chenchen/ccsocket/activities/MainActivity.java
+++ CCSocket/app/src/main/java/com/hello_chenchen/ccsocket/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 //import android.widget.Toast;
 
 import com.hello_chenchen.ccsocket.common.ICommonDefine;
@@ -22,6 +23,16 @@ public class MainActivity extends AppCompatActivity implements ICommonDefine{
         setContentView(R.layout.activity_main);
 
         Button connect_btn = (Button)findViewById(R.id.connect_btn);
+        Button notification_btn = (Button)findViewById(R.id.notification_btn);
+
+        notification_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "默认Toast样式",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         connect_btn.setOnClickListener(new View.OnClickListener() {
             String ipAdress = null;
