63c1f0ac372db13b789db6a4cd3529fd15f629b3
hello_chenchen <welcomechenchen@gmail.com>
2017-02-25 01:30:41 +0800

    实现通知小窗口

--- CCSocket/app/src/main/java/com/hello_chenchen/ccsocket/activities/MainActivity.java
+++ CCSocket/app/src/main/java/com/hello_chenchen/ccsocket/activities/MainActivity.java
@@ -1,5 +1,8 @@
 package com.hello_chenchen.ccsocket.activities;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +20,23 @@ import com.hello_chenchen.ccsocket.base.SocketClient;
 
 public class MainActivity extends AppCompatActivity implements ICommonDefine{
 
+    private void sendNotification() {
+        //获取NotificationManager实例
+        NotificationManager notifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //实例化NotificationCompat.Builde并设置相关属性
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                //设置小图标
+                .setSmallIcon(R.mipmap.ic_launcher)
+                //设置通知标题
+                .setContentTitle("最简单的Notification")
+                //设置通知内容
+                .setContentText("只有小图标、标题、内容");
+        //设置通知时间，默认为系统发出通知的时间，通常不用设置
+        //.setWhen(System.currentTimeMillis());
+        //通过builder.build()方法生成Notification对象,并发送通知,id=1
+        notifyManager.notify(1, builder.build());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +51,8 @@ public class MainActivity extends AppCompatActivity implements ICommonDefine{
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "默认Toast样式",
                         Toast.LENGTH_SHORT).show();
+                sendNotification();
+
             }
         });
 
